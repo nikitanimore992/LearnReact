1efcc9222a5f401055f62c681b88825b616f18f0
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2024-07-04 00:00:56 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+
+
+const box={
+    width:"300px",
+    height:"300px",
+    border:"4px solid Red"
+
+}
+
+const App=()=>{
+    return (
+        <>
+        <center>
+       <h1 style={{width:"300px",height:"300px",border:"4px solid red",backgroundColor:"aqua",borderRadius:"20px", fontSize:"5px"}}><h1 style={{margin:"50px", width:"200px",height:"200px",border:"4px solid green",backgroundColor:"pink",borderRadius:"20px"}}><h1 style={{ padding:"20px 0px 0px 0px", margin:"30px",width:"100px",height:"100px",border:"4px solid red",backgroundColor:"aqua",borderRadius:"20px"}}>Cybrom</h1></h1></h1>
+       </center>
+       
+        </>
+    )
+}
+export default App;
